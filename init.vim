5dcbc88d7d85dc9f5de06199e35e9b994d768bb5
Liam Paul <liam@pop-stack.org>
2021-07-05 11:41:00 -0700

    add lua support

--- init.vim
+++ init.vim
@@ -3,6 +3,7 @@
 " 1.make telescope mapping for adding a file as a split
 " 2.make mapping for closing split without deleting buffer
 
+lua require('lua-ls')
 lua require('plugins')
 lua require('lsp')
 lua require('snipconf')
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -45,7 +45,13 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "clangd", "vimls", "lua", "rust_analyzer"}
+local servers = { "clangd", "vimls", "rust_analyzer" }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach; capabilities = capabilities }
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+       debounce_text_changes = 500,
+    };
+  }
 end
