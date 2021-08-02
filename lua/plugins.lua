a66f74b0b0f13b7d438e1634752b1913582f70c5
Liam Paul <liam@pop-stack.org>
2021-08-02 14:44:08 -0700

    track

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,203 +1,269 @@
+-- auto install packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+-- auto update
 vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 vim.cmd "autocmd BufWritePost plugins.lua call system('[ -f ~/.cache/nvim/update_plugins ] || touch ~/.cache/nvim/update_plugins')"
 vim.cmd "autocmd VimEnter * call Sync()"
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
 
-  -- editing
-  use {
-    -- vim.env.HOME .. "/dev/surround.nvim",
-    'blackCauldron7/surround.nvim',
-    config = function()
-      vim.g.surround_mappings_style = "surround"
-      require "surround".setup {}
-      -- vim.cmd("unmap s")
-    end
-  }
-  use 'b3nj5m1n/kommentary'
-  use {
-    'amadeus/vim-convert-color-to',
-    config=function()
-      vim.api.nvim_set_keymap('n', '<space>cc', ':ConvertColorTo ', {noremap = true})
-    end
-  }
-  use 'hrsh7th/vim-vsnip'
-  use {
-    'windwp/nvim-autopairs',
-    config = function ()
-      require'autopairs'
-      vim.cmd[[
-      function! AuPair()
-      lua reqire 'nvim-autopairs'.autopairs_cr()
-      endfunction
-      ]]
-    end
-  }
-  use {
-    'AckslD/nvim-revJ.lua',
-    requires = {'kana/vim-textobj-user', 'sgur/vim-textobj-parameter'},
-    config = function ()
-      require("revj").setup{
-        keymaps = {
-          operator = '<Leader>J', -- for operator (+motion)
-          line = '<Leader>j', -- for formatting current line
-          visual = '<Leader>j', -- for formatting visual selection
-        }
-      }
-    end
-  }
-  use 'nvim-lua/completion-nvim'
-
-  -- laf
-  use { "rktjmp/lush.nvim" }
-
-  -- "npxbr/gruvbox.nvim",
-  use {
-    '~/dev/gruv.nvim',
-    config = function ()
-      vim.cmd[[colorscheme gruv]]
-    end
-  }
-
-  use {
-    'tjdevries/express_line.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    config = function ()
-      require'el_conf'
-    end
-  }
-
-  use {
-    'romgrk/barbar.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true},
-    config = function ()
-      vim.api.nvim_set_keymap('n', '<space>l', ':BufferNext<cr>', { noremap = true, silent = true})
-      vim.api.nvim_set_keymap('n', '<space>h', ':BufferPrevious<cr>', { noremap = true, silent = true})
-    end
-  }
-
-  -- Navigation
-  use { 'unblevable/quick-scope' }
-
-  use {
-    'nvim-telescope/telescope-fzy-native.nvim',
-    requires = 'romgrk/fzy-lua-native'
-  }
-
-  use { 'nvim-telescope/telescope.nvim',
-    config=function ()
-      require'telescopeConf'
-    end
-  }
-
-  -- backend
-  use 'kabouzeid/nvim-lspinstall'
-  use 'tpope/vim-repeat'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-lua/popup.nvim'
-  use 'airblade/vim-rooter'
-  use 'tpope/vim-sleuth'
-  use {
-    'neovim/nvim-lspconfig',
-    opt = false,
-    config =function ()
-      require'lsp'
-    end
-  }
-  use {
-    'mfussenegger/nvim-dap',
-    config =function ()
-      require'dapConf'
-    end
-  }
-  use 'aymericbeaumet/vim-symlink'
-  use {
-    'RishabhRD/nvim-lsputils',
-    config =function ()
-      require'lsputil_conf'
-    end
-  }
-  use 'RishabhRD/popfix'
-  use 'tjdevries/nlua.nvim'
-
-  -- info
-
-  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-  use { 'p00f/nvim-ts-rainbow' }
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-  use 'kyazdani42/nvim-tree.lua'
-  use 'norcalli/nvim-colorizer.lua'
-  use { 'szw/vim-dict',
-    config = function ()
-      vim.api.nvim_set_keymap(
-        'n',
-        '<space>dic',
-        ':Dict <c-r>=expand("<cword>")<Return><Return>',
-        {noremap=true}
-      )
-    end
-  }
-  use { 'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
+return require('packer').startup({
+  {  -- PLUGIN MANAGER
+     --================--
+    {
+      'wbthomason/packer.nvim',
     },
-    config =function ()
-      require'gitsigns_conf'
-    end
-  }
+    -- TEXT EDITING
+    --==============--
+    {
+      'blackCauldron7/surround.nvim',
+      config = function()
+	vim.g.surround_mappings_style = "surround"
+	require "surround".setup {}
+      end
+    },
+    {
+      'b3nj5m1n/kommentary',
+    },
+    {
+      'amadeus/vim-convert-color-to',
+      config=function()
+	vim.api.nvim_set_keymap('n', '<space>cc', ':ConvertColorTo ', {noremap = true})
+      end
+    },
+    {
+      'hrsh7th/vim-vsnip',
+    },
+    {
+      'windwp/nvim-autopairs',
+      config = function ()
+	require'autopairs'
+	vim.cmd[[
+	function! AuPair()
+	lua reqire 'nvim-autopairs'.autopairs_cr()
+	endfunction
+	]]
+      end
+    },
+    {
+      'AckslD/nvim-revJ.lua',
+      requires = {'kana/vim-textobj-user', 'sgur/vim-textobj-parameter'},
+      config = function ()
+	require("revj").setup {
+	  keymaps = {
+	    operator = '<Leader>J', -- for operator (+motion)
+	    line = '<Leader>j', -- for formatting current line
+	    visual = '<Leader>j', -- for formatting visual selection
+	  }
+	}
+      end
+    },
+    {
+      'nvim-lua/completion-nvim',
+    },
 
-  use { 'nvim-treesitter/nvim-treesitter',
-    config =function ()
-      require'nvim-treesitter.configs'.setup(require'treesitter')
-    end
-  }
-  -- use { 'haringsrob/nvim_context_vt' }
-  use { 'lukas-reineke/indent-blankline.nvim' }
-  use 'vim-scripts/mom.vim'
-use 'tversteeg/registers.nvim'
---[[ use { 'ray-x/lsp_signature.nvim',
-config =function ()
-require'signature_conf'
-end
-  } ]]
-  use { 'sudormrfbin/cheatsheet.nvim',
-    requires = {
-      {'nvim-telescope/telescope.nvim'},
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'},
-    }
-  }
+    -- LAF
+    --=====--
+    {
+      "rktjmp/lush.nvim",
+    },
+    {
+      "npxbr/gruvbox.nvim",
+    },
+    {
+      '~/dev/gruv.nvim',
+      config = function ()
+	vim.cmd[[colorscheme gruv]]
+      end
+    },
+    {
+      'tjdevries/express_line.nvim',
+      requires = 'nvim-lua/plenary.nvim',
+      config = function ()
+	require'el_conf'
+      end
+    },
+    {
+      'romgrk/barbar.nvim',
+      requires = {'kyazdani42/nvim-web-devicons', opt = true},
+      config = function ()
+	vim.api.nvim_set_keymap('n', '<space>l', ':BufferNext<cr>', { noremap = true, silent = true})
+	vim.api.nvim_set_keymap('n', '<space>h', ':BufferPrevious<cr>', { noremap = true, silent = true})
+      end
+    },
 
-  -- other
-  use 'ThePrimeagen/vim-be-good'
-  use { 'akinsho/nvim-toggleterm.lua',
-    config =function ()
-      require'toggleterm_conf'
-    end
-  }
-  use {
-    'TimUntersberger/neogit',
-    requires = {
+    -- NAVIGATION
+    --============--
+    {
+      'unblevable/quick-scope',
+    },
+    {
+      'nvim-telescope/telescope-fzy-native.nvim',
+      requires = 'romgrk/fzy-lua-native'
+    },
+    {
+      'nvim-telescope/telescope.nvim',
+      config=function ()
+	require'telescopeConf'
+      end,
+
+    },
+
+    -- BACKEND
+    --=========--
+    {
+      'kabouzeid/nvim-lspinstall',
+    },
+    {
+      'tpope/vim-repeat',
+    },
+    {
       'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim'
     },
-    config = function ()
-      require'neogit_conf'
-    end
-  }
-  use 'sindrets/diffview.nvim'
-  use 'nvim-treesitter/playground'
+    {
+      'nvim-lua/popup.nvim',
+    },
+    {
+      'airblade/vim-rooter',
+    },
+    {
+      'tpope/vim-sleuth',
+    },
+    {
+      'neovim/nvim-lspconfig',
+      opt = false,
+      config =function ()
+	require'lsp'
+      end
+    },
+    {
+      'mfussenegger/nvim-dap',
+      config =function ()
+	require'dapConf'
+      end
+    },
+    {
+      'aymericbeaumet/vim-symlink',
+    },
+    {
+      'RishabhRD/nvim-lsputils',
+      config =function ()
+	require'lsputil_conf'
+      end
+    },
+    {
+      'RishabhRD/popfix',
+    },
 
-end)
+    -- INFO
+    --======--
+    {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    {
+      'p00f/nvim-ts-rainbow',
+    },
+    {
+      "folke/trouble.nvim",
+      requires = "kyazdani42/nvim-web-devicons",
+      config = function()
+	require("trouble").setup {
+	  --your configuration comes here
+	  --or leave it empty to  the default settings
+	  --refer to the configuration section below
+	}
+      end
+    },
+    {
+      'kyazdani42/nvim-tree.lua',
+    },
+    {
+      'norcalli/nvim-colorizer.lua',
+    },
+    {
+      'szw/vim-dict',
+      config = function ()
+	vim.api.nvim_set_keymap(
+	  'n',
+	  '<space>dic',
+	  ':Dict <c-r>=expand("<cword>")<Return><Return>',
+	  {noremap=true}
+	)
+      end,
+    },
+    {
+      'lewis6991/gitsigns.nvim',
+      requires = {
+	'nvim-lua/plenary.nvim'
+      },
+      config =function ()
+	require'gitsigns_conf'
+      end,
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      config =function ()
+	require'nvim-treesitter.configs'.setup(require'treesitter')
+      end,
+    },
+    {
+      'haringsrob/nvim_context_vt',
+    },
+    {
+      'lukas-reineke/indent-blankline.nvim',
+    },
+    {
+      'vim-scripts/mom.vim',
+    },
+    {
+      'tversteeg/registers.nvim',
+    },
+    { 'ray-x/lsp_signature.nvim',
+      config =function ()
+	require'signature_conf'
+      end
+    },
+    { 'sudormrfbin/cheatsheet.nvim',
+      requires = {
+	{'nvim-telescope/telescope.nvim'},
+	{'nvim-lua/popup.nvim'},
+	{'nvim-lua/plenary.nvim'},
+      }
+    },
 
+    -- OTHER
+    --=======--
+    {
+      'ThePrimeagen/vim-be-good',
+    },
+    {
+      'akinsho/nvim-toggleterm.lua',
+      config =function ()
+	require'toggleterm_conf'
+      end,
+    },
+    {
+      'TimUntersberger/neogit',
+      requires = {
+	'nvim-lua/plenary.nvim',
+	'sindrets/diffview.nvim'
+      },
+      config = function ()
+	require'neogit_conf'
+      end
+    },
+    {
+      'sindrets/diffview.nvim',
+    },
+    {
+      'nvim-treesitter/playground',
+    },
+}, config = {} })
